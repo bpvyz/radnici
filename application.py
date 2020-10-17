4a3c80970edb0a4bb6ecc45f9fd4212f38cb48ae
Bogdan Paunovic <44756741+paunzz@users.noreply.github.com>
2020-10-17 18:58:29 +0200

    asdasdsadasd

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
+import os
+
 from app import create_app
 
 application = create_app()
 
 if __name__ == '__main__':
-    application.secret_key = '123'
+    application.secret_key = os.environ['SECRET_KEY']
     application.run(port=9001)
