517b23290589a6d0af8c7eee8c09e8d94f890b3b
Bogdan Paunovic <44756741+paunzz@users.noreply.github.com>
2020-10-20 16:20:43 +0200

    asdasd

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import os
 from app import create_app
 
 application = create_app()
+application.config.update(SECRET_KEY=os.environ['SECRET_KEY'])
 
 if __name__ == '__main__':
-    application.secret_key = os.environ['SECRET_KEY']
     application.run(port=9001)
